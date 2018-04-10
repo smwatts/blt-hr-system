4b6e74a52c7c2ff3f028f5406ff619200dc51f47
Sarah Watts <smwatts@edu.uwaterloo.ca>
2018-04-10 11:22:38 -0700

    app runs

--- blt_hr_system/settings.py
+++ blt_hr_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lu8mav!a3hy--d)&o7=_2t2$0&w4ji8b^u&+l4_mj7$6_9h^rf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.blt_hr_system.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
