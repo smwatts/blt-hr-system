207fdeee5ff0f3782f3b8835133ffb683e4f7350
Sarah Watts <swatts@bltconstruction.com>
2018-04-29 09:08:31 -0700

    Update settings.py

--- blt_app/blt_app/settings.py
+++ blt_app/blt_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,6 +75,8 @@ WSGI_APPLICATION = 'blt_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
