7ce9f44c13aa1724864eee1337bf6e9b74a19469
Sarah Watts <smwatts@edu.uwaterloo.ca>
2018-05-06 10:31:35 -0700

    forms

--- blt_hr_system/forms.py
+++ blt_hr_system/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from .models import employee_absence, employee_certification
+from django.contrib.admin import widgets
+from bootstrap_datepicker.widgets import DatePicker
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class absence_request(forms.ModelForm):
     class Meta:
         model = employee_absence
-        fields = ['start_date', 'end_date', 'abs_type', 'abs_reason']
+        fields = ['start_date', 'end_date', 'absence_type', 'absence_reason']
+        widgets = {
+            'start_date': DateInput(),
+            'end_date': DateInput(),
+        }
 
 class absence_approval(forms.ModelForm):
     class Meta:
@@ -14,7 +23,11 @@ class absence_approval(forms.ModelForm):
 class cert_request(forms.ModelForm):
     class Meta:
         model = employee_certification
-        fields = ['cert_name', 'acq_date', 'cert_doc']
+        # add the cert name
+        fields = ['acq_date', 'cert_doc']
+        widgets = {
+            'acq_date': DateInput(),
+        }
 
 class cert_approval(forms.ModelForm):
     class Meta:
