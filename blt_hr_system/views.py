979914bdcadb6d46b5eb6ef7c7f6f42e78d9f7c7
Sarah Watts <smwatts@edu.uwaterloo.ca>
2018-07-05 21:52:40 -0700

    added awesomeness

--- blt_hr_system/views.py
+++ blt_hr_system/views.py
@@ -26,14 +26,35 @@ def absence_request(request):
     context = {'absence_form': absence_form}
     return render(request, 'absence_request.html', context)
 
+def add_employee_group(request):
+    if request.method == 'POST':
+        employee_group = forms.add_employee_group(request.POST)
+        employee_group.save()
+        return HttpResponseRedirect(reverse('admin'))
+    else:
+        add_emp_group = forms.add_employee_group()
+    context = {'add_emp_group': add_emp_group}
+    return render(request, 'add_employee_group.html', context)
+
 def training_center(request):
     return render(request, 'training_center.html')
 
-def employee_groups(request):
-    return render(request, 'employee_groups.html')
+def employee_group(request):
+    # Stuck here - can't grab all from employee_group
+    # Could be a problem initializing the table
+    employee_group = models.employee_group.objects.all()
+    emptyCourses = False
+    if not employee_group:
+        emptyCourses = True
+    context = {'employee_group': employee_group,
+               'emptyCourses': emptyCourses}
+    return render(request, 'employee_groups.html', context)
 
 def cert_groups(request):
     return render(request, 'cert_groups.html')
 
 def performance_reviews(request):
     return render(request, 'performance_reviews.html')
+
+def admin(request):
+    return render(request, 'admin.html')
